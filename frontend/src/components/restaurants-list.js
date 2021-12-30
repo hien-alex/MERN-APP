05d1967ba3320126410da6df5546fe3ea957e904
hien-alex <hiennnalex@gmail.com>
2021-12-29 23:36:40 -0800

    setting up react front-end

--- frontend/src/components/restaurants-list.js
+++ frontend/src/components/restaurants-list.js
@@ -1,8 +1,88 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import RestaurantDataService from "../services/restaurant";
 
 const RestaurantsList = (props) => {
+  const [restaurants, setRestaurants] = useState([]);
+  const [searchName, setSearchName] = useState("");
+  const [searchZip, setSearchZip] = useState("");
+  const [searchCuisine, setSearchCuisine] = useState("");
+  const [cuisines, setCuisines] = useState(["All Cuisines"]);
+
+  useEffect(() => {
+    retrieveRestaurants();
+    retrieveCuisines();
+  }, []);
+
+  const onChangeSearchName = (e) => {
+    const searchName = e.target.value;
+    setSearchName(searchName);
+  };
+
+  const onChangeSearchZip = (e) => {
+    const searchZip = e.target.value;
+    setSearchZip(searchZip);
+  };
+
+  const onChangeSearchCuisine = (e) => {
+    const searchCuisine = e.target.value;
+    setSearchCuisine(searchCuisine);
+  };
+
+  const refreshList = () => {
+    setRestaurants([]);
+  };
+
+  const find = (query, by) => {
+    RestaurantDataService.find(query, by)
+      .then((response) => {
+        console.log(response.data);
+        setRestaurants(response.data.restaurants);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  const findName = () => {
+    find(searchName, "name");
+  };
+
+  const findZip = () => {
+    find(searchZip, "zipcode");
+  };
+
+  const findByCuisine = () => {
+    if (setSearchCuisine == "All Cuisines") {
+      retrieveRestaurants();
+    } else {
+      find(searchCuisine, "cuisine");
+    }
+  };
+
+  const retrieveRestaurants = () => {
+    RestaurantDataService.getAll()
+      .then((response) => {
+        console.log(response.data);
+        setRestaurants(response.data.restaurants);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  const retrieveCuisines = () => {
+    RestaurantDataService.getCuisines()
+      .then((response) => {
+        console.log(response.data);
+        setCuisines(cuisines.concat(response.data));
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
   return <div className="restaurants-list">Hello WORLD</div>;
 };
 
