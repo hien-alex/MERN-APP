2dfb11221f147412b9bf9f1039c7b4abdaf3c369
hien-alex <hiennnalex@gmail.com>
2021-12-29 16:42:29 -0800

    setting up react front end and routes

---
+++ frontend/src/components/restaurants-list.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const RestaurantsList = (props) => {
+  return <div className="restaurants-list">Hello WORLD</div>;
+};
+
+export default RestaurantsList;
