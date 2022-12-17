34f74e1dfb526d6238e9d7fbd469f0dd9c92b74a
hien-alex <85124411+hien-alex@users.noreply.github.com>
2022-12-17 12:52:45 -0800

    update cors

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import restaurants from "./api/restaurants.route.js";
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: '*', credentials: true }))
 app.use(express.json());
 
 app.use("/api/v1/restaurants", restaurants);
