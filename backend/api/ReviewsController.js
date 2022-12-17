0d9838f4491a391c845f54c61981c07fc26654bf
hien-alex <85124411+hien-alex@users.noreply.github.com>
2022-12-17 13:25:15 -0800

    remove console.log

--- backend/api/ReviewsController.js
+++ backend/api/ReviewsController.js
@@ -3,7 +3,6 @@ import reviewsDAO from "./DAO/reviewsDAO.js";
 export default class ReviewsController {
   static async apiPostReview(req, res, next) {
     try {
-      console.log(req.body.restaurant_id);
       const restaurantId = req.body.restaurant_id;
       const review = req.body.text;
       const userInfo = {
